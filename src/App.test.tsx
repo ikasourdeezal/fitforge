406ddc0b783519d757dfaa47d6aec7dfdd95b047
ikasourdeezal <ikasourdeezal@gmail.com>
2026-02-12 01:20:21 +0400

    feat: implement automated testing and pre-commit hooks

---
+++ src/App.test.tsx
@@ -0,0 +1,16 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+
+test("renders FitForge landing page without crashing", () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  );
+
+  // Replace 'FitForge' with whatever unique text is in your Header
+  const linkElement = screen.getByText(/find your energy/i);
+  expect(linkElement).toBeInTheDocument();
+});
